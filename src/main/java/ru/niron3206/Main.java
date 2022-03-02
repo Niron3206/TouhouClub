582995074d82afdbd0e778fbfb35c2c6f3f76b39
klen-list <anton.burkov.02@mail.ru>
2022-03-02 21:59:59 +0300

    Fix members cache

--- src/main/java/ru/niron3206/Main.java
+++ src/main/java/ru/niron3206/Main.java
@@ -3,6 +3,9 @@ package ru.niron3206;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 import javax.security.auth.login.LoginException;
 
@@ -13,6 +16,9 @@ public class Main {
     public static void main(String[] args) throws LoginException {
         jda = JDABuilder.createDefault(Config.get("TOKEN"))
                 .setActivity(Activity.listening("my master."))
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .setChunkingFilter(ChunkingFilter.ALL)
                 .build();
         jda.addEventListener(new Listener());
     }
